beff802bc2835d431a6fe552949d36ae19576203
hacker2889 <3331497264@qq.com>
2022-10-07 20:29:28 +0800

    x pow

---
+++ homework/x的n次方.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//double my_Pow(int x, int n)
+//{
+//	if (n == 0)
+//		return 1.0;
+//	else if (n > 0)
+//		return x * my_Pow(x, n - 1);
+//		
+//	else/*
+//		return 1.0/ (my_Pow(x, -n));
+//}
+//int main()
+//{
+//	int x = 0;
+//	int n = 0;
+//	scanf("%d %d", &x, &n);
+//	double ret =my_Pow(x, n);
+//	printf("%lf\n", ret);
+//	return 0;
+//}*/
