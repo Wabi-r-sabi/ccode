986af0b6d301b044c798fcf4d8a70d9f33b1e16a
hacker2889 <3331497264@qq.com>
2022-10-12 23:37:41 +0800

    recursion

---
+++ homework/递推求圆周率.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//    int i = 2, f = 1;
+//    double pi, sum = 0, t = 1;
+//    while (t >= 1e-6)
+//    {
+//        sum = sum + t*f;
+//        t = 1.0/ (2 * i - 1);
+//        i++;
+//        f = -f;
+//    }
+//    pi = sum * 4;
+//    printf("pi=%lf\n", pi);
+//    return 0;
+//}
