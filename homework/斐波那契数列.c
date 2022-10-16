ce78ddde97a774b4e7a9a61d7453c9fcff2e7c78
hacker2889 <3331497264@qq.com>
2022-10-16 14:47:46 +0800

    Fib

---
+++ homework/斐波那契数列.c
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int Fib(int n)
+//{
+//	int ret = 0;
+//	if (n <= 2)
+//	{
+//		return ret = 1;
+//	}
+//	else
+//	{
+//		return ret = Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+//	
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		int ret = Fib(i);
+//		printf("%d ", ret);
+//	}
+//	return 0;
+//}
---
+++ homework/斐波那契数列1.c
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int fbnq(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = a;
+//	while (n > 2)
+//	{
+//		c = a + b;//从第三个数开始，斐波那契数等于前两个数的和
+//		a = b;//将前一个数给a,求下一个值
+//		b = c;//将斐波那契数给b求下一个值
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int m = 40;
+//	for (int i = 1; i <= m; i++)
+//	{
+//		int ret = fbnq(i);
+//		printf("%d ", ret);
+//	}
+//	printf("\n");
+//	return 0;
+//}
