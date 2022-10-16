00e1d636fb2e08772d0177550173d5f63d4e9f14
hacker2889 <3331497264@qq.com>
2022-10-16 20:03:29 +0800

    function

---
+++ homework/分段函数.c
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int sign(int x)
+//{
+//	if (x < 0)
+//	{
+//		return -1;
+//	}
+//	else if (x > 0)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	int x = 0;
+//	printf("«Î ‰»Îxµƒ÷µ:");
+//	scanf("%d",&x);
+//	int ret = sign(x);
+//	printf("%d\n", ret);
+//}
