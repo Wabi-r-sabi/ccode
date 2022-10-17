da387fcb93fee45c8cae6028cfa81169a1144fb8
hacker2889 <3331497264@qq.com>
2022-10-17 20:34:53 +0800

    distance

---
+++ homework/两点之间的距离.c
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//double Distance(double,double,double,double);
+//double Distance(double x1, double y1, double x2, double y2)
+//{
+//	if ((x1 - x2) * (x1 - x2) - (y1 - y2) * (y1 - y2) >= 0)
+//	{
+//		return sqrt((x1 - x2) * (x1 - x2) - (y1 - y2) * (y1 - y2));
+//	}
+//	else
+//	{
+//		exit(0);
+//	}
+//}
+//int main()
+//{
+//	double x1 = 0.0;
+//	double x2 = 0.0;
+//	double y1 = 0.0;
+//	double y2 = 0.0;
+//	printf("请输入点的坐标:");
+//	scanf("%lf %lf %lf %lf",&x1,&x2,&y1,&y2);
+//	printf("这两点之间的距离为:%.2f\n", Distance(x1, y1, x2, y2));
+//}
